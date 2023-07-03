cdcb1db990164d9e795b9a35e61190783f61b14d
Hemanthgollu <138460051+Hemanthgollu@users.noreply.github.com>
2023-07-03 16:07:27 +0530

    Create greatest of the three no's.py

---
+++ greatest of the three no's.py
@@ -0,0 +1,9 @@
+x = int(input('enter the 1st value'))
+y = int(input('enter the 2nd value'))
+z = int(input('enter the 3rd value'))
+if x > y and x > z:
+    print(x)
+elif y > z:
+    print(y)
+else:
+    print(z)
