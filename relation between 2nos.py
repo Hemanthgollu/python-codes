fbb98101c74d98ceed0bd056d67027dc5029c618
Hemanthgollu <138460051+Hemanthgollu@users.noreply.github.com>
2023-07-04 10:29:26 +0530

    Create relation between 2nos.py

---
+++ relation between 2nos.py
@@ -0,0 +1,8 @@
+x = int(input('enter the 1st value '))
+y = int(input('enter the 2nd value'))
+if x > y:
+    print('x is greater than y')
+elif x == y:
+    print('y is eqaul to x')
+else:
+    print('y is greater than x')
