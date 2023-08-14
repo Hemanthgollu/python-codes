a957b7b7dda5f5f5d59b90a8f909cbe55251be64
Hemanthgollu <138460051+Hemanthgollu@users.noreply.github.com>
2023-08-14 23:11:47 +0530

    Create recursive.py

---
+++ recursive.py
@@ -0,0 +1,17 @@
+#recursive sum method
+def sdigit(y):
+    
+    r = 0
+    for i  in y:
+        r = r+int(i)
+    if len(str(r))==1:
+        return r
+    else:
+        return sdigit(str(r))
+           
+    
+
+x,k = input().split()
+m = sdigit(x)*int(k)
+print(sdigit(str(m)))
+
