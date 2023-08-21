2fd329f5176d1d94cca94ba20f0cbd2740599205
Hemanthgollu <138460051+Hemanthgollu@users.noreply.github.com>
2023-08-21 22:03:24 +0530

    Create areaofcircle.py

---
+++ areaofcircle.py
@@ -0,0 +1,7 @@
+#to find area of circle
+def circ_Area(rad):
+	PI = 3.142
+	return PI * (rad*rad);
+
+rad = float(input('Enter the radius of the circle: '))
+print("Area of the circle is %.6f" % circ_Area(rad));
