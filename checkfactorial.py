7106ff770dced979bfddac81d9a3b93268f65a2e
Hemanthgollu <138460051+Hemanthgollu@users.noreply.github.com>
2023-08-22 23:15:55 +0530

    Create checkfactorial.py

---
+++ checkfactorial.py
@@ -0,0 +1,7 @@
+#factorial of a given no
+import math
+def factorial(num):
+	return(math.factorial(num))
+num = int(input('Please enter a number to find the factorial: '))
+print("The factorial of the given number", num, "is",
+	factorial(num))
